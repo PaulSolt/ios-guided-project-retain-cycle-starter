0806edb93c63509005e7ae5a4151d47389d61526
Paul Solt <PaulSolt@gmail.com>
2020-04-23 14:10:58 -0400

    Added UI and updated interfaace

--- iOS14-XKCD/iOS14-XKCD/ViewController.m
+++ iOS14-XKCD/iOS14-XKCD/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import "LSIComic.h"
 
 @interface ViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -26,8 +29,81 @@
     
     // 2. download image
     
+    NSURL *baseURL = [NSURL URLWithString:@"https://xkcd.com/"];
     
+    NSString *endPoint = @"info.0.json";
+    int comicNumber = 2261; // 417 (portait)
     
+    BOOL mostRecent = NO;
+    
+    NSURL *url = [baseURL URLByAppendingPathComponent:endPoint]; // http://xkcd.com/info.0.json
+    
+    if (!mostRecent) { // http://xkcd.com/2261/info.0.json
+        NSString *comicNumberString = [NSString stringWithFormat:@"%i", comicNumber];
+        url = [[baseURL URLByAppendingPathComponent: comicNumberString] URLByAppendingPathComponent:endPoint];
+    }
+    
+    [[NSURLSession.sharedSession dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"Url: %@", url);
+        
+        if (error) {
+            NSLog(@"Error: %@", error);
+            return;
+        }
+        
+        // TODO: data is not nil
+        
+        // sometimes you must check for type
+        // isKindOfClass method
+        
+        NSError *jsonError = nil;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&error];
+
+        if (jsonError) {
+            NSLog(@"JSON Error: %@", jsonError);
+            return;
+        }
+
+        LSIComic *comic = [[LSIComic alloc] initWithDictionary:json];
+
+        NSLog(@"comic: %@", comic.imageURL);
+        
+        // Display text content
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.titleLabel.text = comic.title;
+        });
+
+        [self downloadImage:comic.imageURL];
+        
+    }] resume];
+    
+}
+
+- (void)downloadImage:(NSURL *)imageURL {
+    [[NSURLSession.sharedSession dataTaskWithURL:imageURL completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"imageURL: %@", imageURL);
+        
+        if (error) {
+            NSLog(@"Error: %@", error);
+            return;
+        }
+        
+        // TODO: data is not nil
+        
+        // NSData object with image data
+        UIImage *image = [UIImage imageWithData:data];
+        
+        NSLog(@"image.size: %@", NSStringFromCGSize(image.size));
+        
+        // Display image based content
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            self.imageView.image = image;
+        });
+
+    }] resume];
 }
 
 
