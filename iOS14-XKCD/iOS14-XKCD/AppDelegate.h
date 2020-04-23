77cd898b48425de4e28add8ca7b90febef4d10fb
Paul Solt <PaulSolt@gmail.com>
2020-04-23 13:36:09 -0400

    Added starter project Added build scheme

---
+++ iOS14-XKCD/iOS14-XKCD/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS14-XKCD
+//
+//  Created by Paul Solt on 4/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ iOS14-XKCD/iOS14-XKCD/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iOS14-XKCD
+//
+//  Created by Paul Solt on 4/23/20.
+//  Copyright © 2020 Lambda, Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
