a6e5d76d6a13bc2a4c94c9ad3a679ccb3dc51a0c
Paul Solt <PaulSolt@gmail.com>
2020-05-21 13:51:01 -0400

    Parsing comic

--- iOS15-XKCD/iOS15-XKCD/ViewController.m
+++ iOS15-XKCD/iOS15-XKCD/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "LSIComic.h"
 
 @interface ViewController ()
 
@@ -23,7 +24,44 @@
     
     // Network call
     
+    //    1. Download JSON
+    // 2. Download image
+
+    NSURL *baseURL = [NSURL URLWithString:@"https://xkcd.com/"];
     
+    NSString *endPoint = @"info.0.json";
+    int comicNumber = 2261; // 417 (portait)
+    
+    BOOL mostRecent = NO;
+    
+    NSURL *url = [baseURL URLByAppendingPathComponent:endPoint]; // http://xkcd.com/info.0.json
+    
+    if (!mostRecent) { // http://xkcd.com/2261/info.0.json
+        NSString *comicNumberString = [NSString stringWithFormat:@"%i", comicNumber];
+        url = [[baseURL URLByAppendingPathComponent: comicNumberString] URLByAppendingPathComponent:endPoint];
+    }
+
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"url: %@", url);
+        
+        if (error) { // error != nil // is the error non-zero
+            NSLog(@"Error downloading comic json: %@", error);
+        }
+        
+        NSError *jsonError = nil;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        
+        if (jsonError) {
+            NSLog(@"JSON error: %@", error);
+        }
+        
+        LSIComic *comic = [[LSIComic alloc] initWithDictionary:json];
+        
+        NSLog(@"comic %@", comic.imageURL);
+        
+    }];
+    [task resume];
     
 }
 
