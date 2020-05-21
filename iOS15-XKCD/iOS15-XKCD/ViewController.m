1a1012509b46aca9eb058dd60f9a6c8ea97f9a37
Paul Solt <PaulSolt@gmail.com>
2020-05-21 13:59:31 -0400

    Setup UI

--- iOS15-XKCD/iOS15-XKCD/ViewController.m
+++ iOS15-XKCD/iOS15-XKCD/ViewController.m
@@ -9,7 +9,12 @@
 #import "ViewController.h"
 #import "LSIComic.h"
 
+// Class extension
 @interface ViewController ()
+
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+
+@property (strong, nonatomic) IBOutlet UIImageView *imageView;
 
 @end
 
@@ -60,10 +65,48 @@
         
         NSLog(@"comic %@", comic.imageURL);
         
+        // update the UI
+        dispatch_async(dispatch_get_main_queue(), ^{
+//            self.comic = comic;
+//            [self updateViews];
+            self.titleLabel.text = comic.title;
+        });
+        // make 2nd request
+        [self downloadImageWithURL: comic.imageURL];
     }];
     [task resume];
-    
 }
 
+- (void)updateViews {
+    // TODO: pull in data
+}
+
+- (void)downloadImageWithURL:(NSURL *)url {
+    NSURLSessionDataTask *task = [[NSURLSession sharedSession] dataTaskWithURL:url completionHandler:^(NSData * _Nullable data, NSURLResponse * _Nullable response, NSError * _Nullable error) {
+        
+        NSLog(@"url: %@", url);
+        
+        if (error) { // error != nil // is the error non-zero
+            NSLog(@"Error downloading image: %@", error);
+        }
+        
+        NSError *jsonError = nil;
+        NSDictionary *json = [NSJSONSerialization JSONObjectWithData:data options:0 error:&jsonError];
+        
+        if (jsonError) {
+            NSLog(@"JSON error: %@", error);
+        }
+        
+        UIImage *image = [UIImage imageWithData:data]; // TODO: check it's valid
+        
+        // update the UI
+        dispatch_async(dispatch_get_main_queue(), ^{
+            // [self updateViews];
+            self.imageView.image = image;
+        });
+        
+    }];
+    [task resume];
+}
 
 @end
